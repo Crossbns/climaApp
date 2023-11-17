18bf71cb254ba6ce434e56c7a27706aa15f90c34
César Márquez <cesar.marquezsal@gmail.com>
2023-11-17 08:48:51 -0400

    new design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { LoadingButton } from "@mui/lab";
 import { Box, Container, TextField, Typography } from "@mui/material";
 import { useState } from "react";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { Grid } from '@mui/material';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Grid } from "@mui/material";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
 
 const API_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${
   import.meta.env.VITE_API_KEY
@@ -14,21 +14,24 @@ const API_WEATHER = `https://api.weatherapi.com/v1/current.json?key=${
 //Dark Mode
 const darkTheme = createTheme({
   typography: {
-    fontFamily: 'Poppins, sans-serif',
+    fontFamily: "Poppins, sans-serif",
   },
   palette: {
-    mode: 'dark',
+    mode: "dark",
     primary: {
-      main: '#4374fa',
+      main: "#fcfcfc",
     },
     secondary: {
-      main: '#03a9f4',
+      main: "#03a9f4",
     },
     background: {
-      default: '#11151f',
+      default: "#2f363d",
     },
     boxBackground: {
-      main: '#2b3040',
+      main: "#2b3040",
+    },
+    border: {
+      main: "#fcfcfc", // same as secondary color
     },
   },
 });
@@ -36,25 +39,30 @@ const darkTheme = createTheme({
 //Light Mode
 const lightTheme = createTheme({
   typography: {
-    fontFamily: 'Poppins, sans-serif',
+    fontFamily: "Poppins, sans-serif",
   },
   palette: {
-    mode: 'light',
+    mode: "light",
     primary: {
-      main: '#4374fa',
+      main: "#2b2b2b",
     },
     secondary: {
-      main: '#03a9f4',
+      main: "#fcfcfc",
+    },
+    background: {
+      default: "#edf3f5",
     },
     boxBackground: {
-      main: '#e0e0e0',
+      main: "#e0e0e0",
+    },
+    border: {
+      main: "#2b2b2b",
     },
   },
 });
 
 //Main Component
 export default function App() {
-
   //States definition
   const [city, setCity] = useState("");
   const [error, setError] = useState({
@@ -62,11 +70,11 @@ export default function App() {
     message: "",
   });
   const [loading, setLoading] = useState(false);
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState("dark");
 
   //Function to change the theme
   const toggleTheme = () => {
-    setTheme(theme === 'dark' ? 'light' : 'dark');
+    setTheme(theme === "dark" ? "light" : "dark");
   };
 
   const [weather, setWeather] = useState({
@@ -109,7 +117,6 @@ export default function App() {
         windSpeed: data.current.wind_kph,
         uvIndex: data.current.uv,
       });
-      
     } catch (error) {
       console.log(error);
       setError({ error: true, message: error.message });
@@ -117,32 +124,31 @@ export default function App() {
       setLoading(false);
     }
   };
-  
-//Renderizing components
+
+  //Renderizing components
   return (
-    <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
+    <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
       <CssBaseline />
-  
+
       <Box
         sx={{
-          display: 'flex',
-          justifyContent: 'flex-end',
+          display: "flex",
+          justifyContent: "flex-end",
           ml: { xs: 2, sm: 3, md: 4, lg: 5, xl: 6 },
         }}
       >
         <LoadingButton onClick={toggleTheme}>
-          {theme === 'dark' ? (
+          {theme === "dark" ? (
             <Brightness7Icon fontSize="large" />
           ) : (
             <Brightness4Icon fontSize="large" />
           )}
         </LoadingButton>
       </Box>
-  
-      <Container maxWidth="xs" sx={{ mt: 2 }}>
 
+      <Container maxWidth="xs" sx={{ mt: 2 }}>
         <Box
-          sx={{ display: 'grid', gap: 2 }}
+          sx={{ display: "grid", gap: 2 }}
           component="form"
           autoComplete="off"
           onSubmit={onSubmit}
@@ -151,52 +157,88 @@ export default function App() {
             id="city"
             label="City"
             variant="outlined"
-            size="small"react
+            size="small"
             required
             value={city}
             onChange={(e) => setCity(e.target.value)}
             error={error.error}
             helperText={error.message}
+            sx={{
+              marginBottom: "30px",
+            }}
           />
-  
-          <LoadingButton
-            type="submit"
-            variant="contained"
-            loading={loading}
-            loadingIndicator="Searching..."
-            sx={{ fontSize: { xs: '1rem', sm: '1.25rem', md: '1.5rem' }, fontWeight: 'bold' }}
-          >
-            Search
-          </LoadingButton>
         </Box>
-  
+
         {weather.city && (
           <Box sx={{ mt: 2 }}>
-            <Typography variant="h4" component="h2" align="center">
+            <Typography
+              variant="h4"
+              component="h2"
+              align="center"
+              sx={{ mb: 2 }}
+            >
               {weather.city}, {weather.country}
             </Typography>
             <Box
               component="img"
               alt={weather.conditionText}
               src={weather.icon}
-              sx={{ display: 'block', margin: '0 auto' }}
+              sx={{
+                display: "block",
+                margin: "0 auto",
+                width: "100px",
+                height: "100px",
+                mb: 2,
+              }}
             />
-            <Typography variant="h5" component="h3" align="center">
-              {weather.temperature} °C
-            </Typography>
-            <Grid container spacing={10}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Typography
+                variant="h5"
+                component="h3"
+                align="center"
+                sx={{
+                  fontSize: "3rem",
+                  fontWeight: "bold",
+                  mb: 5,
+                  ml: 2.5,
+                  borderBottom: `1px solid ${
+                    theme === "dark"
+                      ? darkTheme.palette.border.main
+                      : lightTheme.palette.border.main
+                  }`,
+                  width: "40%",
+                }}
+              >
+                {weather.temperature} °C
+              </Typography>
+            </Box>
+
+            <Grid
+              container
+              spacing={1}
+              alignItems="center"
+              justifyContent="center"
+            >
               <Grid item xs={12} sm={4}>
                 <Box
                   sx={{
                     p: 1,
-                    bgcolor: (theme) => theme.palette.boxBackground.main,
-                    borderRadius: 2,
-                    width: { xs: '100%', sm: 150 },
+                    width: { xs: "100%", sm: 150 },
                     height: { xs: 100, sm: 80 },
                   }}
                 >
-                  <Typography variant="body1" component="p" align="center">
-                    <strong style={{ textDecoration: 'underline' }}>Condition</strong>: {weather.conditionText}
+                  <Typography variant="body2" component="p" align="center">
+                    <strong style={{ textDecoration: "underline" }}>
+                      Condition
+                    </strong>
+                    : {weather.conditionText}
                   </Typography>
                 </Box>
               </Grid>
@@ -204,15 +246,32 @@ export default function App() {
                 <Box
                   sx={{
                     p: 1,
-                    bgcolor: (theme) => theme.palette.boxBackground.main,
-                    borderRadius: 2,
-                    width: { xs: '100%', sm: 150 },
-                    height:{ xs: 100, sm: 75 },
-                    marginBottom:{ xs: 1, sm:'auto' }
+                    width: { xs: "100%", sm: 150 },
+                    height: { xs: 100, sm: 75 },
+                    marginBottom: { xs: 1, sm: "auto" },
                   }}
                 >
-                  <Typography variant="body1" component="p" align="center">
-                    <strong style={{ textDecoration: 'underline' }}>Wind Speed</strong>: {weather.windSpeed} km/h
+                  <Typography
+                    variant="body2"
+                    component="p"
+                    align="center"
+                    style={{
+                      borderLeft: `0.5px solid ${
+                        theme === "dark"
+                          ? darkTheme.palette.border.main
+                          : lightTheme.palette.border.main
+                      }`,
+                      borderRight: `0.5px solid ${
+                        theme === "dark"
+                          ? darkTheme.palette.border.main
+                          : lightTheme.palette.border.main
+                      }`,
+                    }}
+                  >
+                    <strong style={{ textDecoration: "underline" }}>
+                      Wind Speed
+                    </strong>
+                    : {weather.windSpeed} km/h
                   </Typography>
                 </Box>
               </Grid>
@@ -220,29 +279,36 @@ export default function App() {
                 <Box
                   sx={{
                     p: 1,
-                    bgcolor:(theme) => theme.palette.boxBackground.main,
-                    borderRadius: 2,
-                    width:{ xs:'100%', sm :150 },
-                    height:{ xs :100, sm :75 }
+                    width: { xs: "100%", sm: 150 },
+                    height: { xs: 100, sm: 75 },
                   }}
                 >
-                  <Typography variant="body1" component="p" align="center">
-                   <strong style={{ textDecoration:'underline' }}>UV Index</strong>: {weather.uvIndex}
+                  <Typography variant="body2" component="p" align="center">
+                    <strong style={{ textDecoration: "underline" }}>
+                      UV Index
+                    </strong>
+                    : {weather.uvIndex}
                   </Typography>
                 </Box>
               </Grid>
             </Grid>
           </Box>
         )}
-  
-        <Typography textAlign='center' sx={{ mt :2, fontSize:{ xs:'8px', sm:'10px' } }}>
-          Powered by:{' '}
-          <a href="<URL>" title="<URL>">
+
+        <Typography
+          textAlign="center"
+          sx={{ mt: 2, fontSize: { xs: "8px", sm: "10px" } }}
+        >
+          Powered by:{" "}
+          <a
+            href="<URL>"
+            title="<URL>"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
             WeatherAPI.com
           </a>
         </Typography>
       </Container>
     </ThemeProvider>
   );
-                
-}  
+}
